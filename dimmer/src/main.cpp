7d3846747959ed41e1e397a7a717f2c5bcd107d7
stuhlmeier <stuhlmeier@gmail.com>
2018-12-16 09:09:22 +0100

    Add dimmer task

--- dimmer/src/main.cpp
+++ dimmer/src/main.cpp
@@ -1,8 +1,10 @@
 #include <wiringPi.h>
 #include <softPwm.h>
+#include <ads1115.h>
 
 #include <constants.h>
 
+#include <algorithm>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
@@ -10,16 +12,21 @@
 #include <iostream>
 
 const int R = 0, G = 1, B = 2;
-const int P = 3;
+const int P = 0;
+
+const int ADS1115_OFFSET = 128;
+const int ADS1115_ADDRESS = 0x48;
 
 void setup() {
+    ads1115Setup(ADS1115_OFFSET, ADS1115_ADDRESS);
+    
     // The LED
     softPwmCreate(R, 0, 100);
     softPwmCreate(G, 0, 100);
     softPwmCreate(B, 0, 100);
     
     // The photoresistor
-    pinMode(P, INPUT);
+    pinMode(ADS1115_OFFSET + P, INPUT);
 }
 
 std::int64_t timer;
@@ -27,14 +34,18 @@ std::int64_t cycleTimer;
 
 int ledStrength;
 
+const float CLAMP_MIN = 6000;
+const float CLAMP_MAX = 26000;
+
 void loop(std::int64_t delta) {
     timer += delta;
     cycleTimer += delta;
     
     if (cycleTimer >= 10000) {
-        auto seconds = timer / 1000000.0f;
-        auto value = std::sin(PI<float> * seconds);
-        auto scaled = (value + 1) * 20;
+        auto value = static_cast<float>(analogRead(ADS1115_OFFSET + P));
+        auto strength = std::clamp(value, CLAMP_MIN, CLAMP_MAX) - CLAMP_MIN;
+
+        auto scaled = strength / (CLAMP_MAX - CLAMP_MIN) * 100.0f;
         
         ledStrength = static_cast<int>(scaled);
     
